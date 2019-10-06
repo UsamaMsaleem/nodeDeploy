ee01a0de1f959f20c7cf3d41eeca0077f72396a7
UsamaSaleem123 <pepsi.killer.pk@gmail.com>
2019-10-07 04:03:58 +0500

    add all the file

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const app = express();
 const mongoose =require('./Config/db')
+var cors = require('cors')
 const db = mongoose.connection
 const port =process.env.PORT || 3001;
+// var cors = require('cors')
 
 db.once('open',()=>{
     console.log('db connected')
@@ -14,10 +16,23 @@ app.listen(port, () => {
 .on("error",error => {
     console.log("error",error)
 })
+// app.use(cors({
+//     origin: 'http://localhost',
+//     allowedHeaders: ["Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"]
+// }));
 
+// app.options('*', cors())
+
+app.use(cors())
 // app.get('/',(req,res)=>{
 //     console.log("helloworld2")
 // })
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//  });
 
 app.get('/GetAllFriends',(req,res)=>{
     // console.log("helloworld2")
